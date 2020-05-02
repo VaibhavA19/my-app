9688371aeb2d5318345498346f47eb1d19c479f2
VaibhavA19 <alreja6@gmail.com>
2020-05-02 13:32:40 -0700

    Updates

---
+++ precache-manifest.f93d4a713ac69df6a63c19cda1a6726a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c681af85f6166255c130600c9e8a40dc",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "3a6b619420e3c4bbdfd0",
+    "url": "/my-app/static/css/main.594d6d57.chunk.css"
+  },
+  {
+    "revision": "4f60aad9809034c6d270",
+    "url": "/my-app/static/js/2.93139a8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.93139a8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a6b619420e3c4bbdfd0",
+    "url": "/my-app/static/js/main.1f03c329.chunk.js"
+  },
+  {
+    "revision": "7017a0a3783b2a9ce739",
+    "url": "/my-app/static/js/runtime-main.b51f6de3.js"
+  }
+]);
